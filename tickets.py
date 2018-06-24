ea4b778f35bfa7bf3eac37f9ed3c4c03e8478246
Chaos_Judice <32519088+VincentAugustine@users.noreply.github.com>
2018-06-24 21:33:43 +0800

    Version 10000

--- tickets.py
+++ tickets.py
@@ -23,18 +23,54 @@ import requests
 from pprint import pprint
 from station import station
 from docopt import docopt
+from prettytable import PrettyTable
+from colorama import init,Fore
+
+init()
 
 class TrainsCollection:
     header = "车次 车站 时间 历时 商务座 一等 二等 高级软卧 软卧 动卧 硬卧 软座 硬座 无座".split()
 
-    def __init__(self, trains, options):
-        
+    def __init__(self, available_trains, options):
+        self.available_trains = available_trains
+        self.options = options
+
+    def _get_duration(self,duration):
+        duration = duration.replace(":","小时")+"分"
+        if duration.startswith("00"):
+            return duration[4:]
+        if duration.startswith("0"):
+            return duration[3:]
+        return duration
+
+    def trains(self):
+        res = []
+        for available in self.available_trains:
+            res.append([
+                available["train_no"],Fore.GREEN+available["from_station"]+Fore.RESET+ "\n" + Fore.RED+available["to_station"]+Fore.RESET,
+                Fore.GREEN+available["start_time"] +Fore.RESET+ "\n" +Fore.RED+available["end_time"]+Fore.RESET,  self._get_duration(available["duration"]), available["business_seat"],
+                available["first_seat"],available["second_seat"],available["gjrw"],available["rw"],available["dw"],
+                available["yw"], available["rz"],available["yz"],available["wz"]
+
+            ])
+        return res
+    11
+    def pretty_print(self):
+        pt = PrettyTable()
+        pt._set_field_names(self.header)#加表头
+        for train in self.trains():
+            pt.add_row(train)
+        print(pt)
+
 
 def cli():
     arguement = docopt(__doc__)
     fstation = station.get(arguement['<from>'])
     tstation = station.get(arguement['<to>'])
     date = arguement['<date>']
+    options = ''.join(
+        [key for key, value in arguement.items() if value is True]
+    )
     get_station_url = "https://kyfw.12306.cn/otn/leftTicket/query?leftTicketDTO.train_date={}&leftTicketDTO.from_station={}&leftTicketDTO.to_station={}&purpose_codes=ADULT".format(
         date, fstation, tstation)
     r = requests.get(get_station_url)
@@ -71,10 +107,17 @@ def cli():
                 "from_station_no": r_list[16],
                 "destinction_no": r_list[17],
             }
+            for key,value in r_dict.items():
+                if value == "":
+                    r_dict[key] = "--"
+
             trains.append(r_dict)
-        print(trains)
+
+        t = TrainsCollection(trains,options)
+        t.pretty_print()
 
     except BaseException:
+
         print('Bye')
 
 
